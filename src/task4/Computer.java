612cea4a41322139140c178e90babe22a5ef7f12
myGHac <oursecondmail@ukr.net>
2020-11-22 11:11:55 +0200

    editet project

--- src/task4/Computer.java
+++ src/task4/Computer.java
@@ -2,5 +2,44 @@ package task4;
 
 public class Computer {
 
+  public String cpuName;
+  public String sysBoard;
+  public String ramVolume;
+  public String hdVolume;
 
+   // public  Computer() { };
+  //public  Computer(String cpuName, String sysBoard, String ramVolume, String hdVolume) {
+   // }
+
+    public String getCpuName() {
+        return cpuName;
+    }
+
+    public void setCpuName(String cpuName) {
+        this.cpuName = cpuName;
+    }
+
+    public String getSysBoard() {
+        return sysBoard;
+    }
+
+    public void setSysBoard(String sysBoard) {
+        this.sysBoard = sysBoard;
+    }
+
+    public String getRamVolume() {
+        return ramVolume;
+    }
+
+    public void setRamVolume(String ramVolume) {
+        this.ramVolume = ramVolume;
+    }
+
+    public String getHdVolume() {
+        return hdVolume;
+    }
+
+    public void setHdVolume(String hdVolume) {
+        this.hdVolume = hdVolume;
+    }
 }
--- src/task4/Main.java
+++ src/task4/Main.java
@@ -2,43 +2,57 @@ package task4;
 
 import task4.Computer;
 
-import java.util.Scanner;
-
-
-
-
 public class Main {
+
+public static void computers() {
+
+    Computer [] computers = new Computer[5];
+
+    computers[0] = new Computer();
+    computers[1] = new Computer();
+    computers[2] = new Computer();
+    computers[3] = new Computer();
+    computers[4] = new Computer();
+
+    computers[0].cpuName ="Intel core i3 ";
+    computers[0].sysBoard ="Asus P8H61-M";
+    computers[0].ramVolume = "8 Gb";
+    computers[0].hdVolume =" 500 Gb ";
+
+    computers[1].cpuName = "Intel core i5 ";
+    computers[1].sysBoard ="Asus P8H61-M LX2";
+    computers[1].ramVolume ="16 Gb";
+    computers[1].hdVolume =" 1000 Gb ";
+
+    computers[2].cpuName="Intel core i7 ";
+    computers[2].sysBoard ="Asus P8H61-M LE2";
+    computers[2].ramVolume = "16 Gb";
+    computers[2].hdVolume = " 500 Gb ";
+
+    computers[3].cpuName = "Intel G2020 ";
+    computers[3].sysBoard ="Asus P8H68-M";
+    computers[3].ramVolume = "4 Gb";
+    computers[3].hdVolume = " 1000 Gb ";
+
+    computers[4].cpuName = "Intel G2030 ";
+    computers[4].sysBoard = "Asus P8H68-M LE";
+    computers[4].ramVolume = "16 Gb";
+    computers[4].hdVolume=" 500 Gb ";
+
+    for (int i = 1; i < computers.length; i++) {
+
+        computers[i].setCpuName(computers[i].cpuName);
+        computers[i].setSysBoard(computers[i].sysBoard);
+        computers[i].setRamVolume(computers[i].ramVolume);
+        computers[i].setHdVolume(computers[i].hdVolume);
+
+        System.out.println("Обьект " + i + " записан.");
+    }
+}
 
     public static void main(String[] args) {
 
-//        System.out.println("Введите название книги: ");
-//        Scanner sc = new Scanner(System.in);
-//        String title  = sc.nextLine();
-//
-//        System.out.println("Введите фамилию и инициалы автора: ");
-//        sc = new Scanner(System.in);
-//        String author = sc.nextLine();
-//
-//        System.out.println("Введите краткое содержание: ");
-//        sc = new Scanner(System.in);
-//        String content = sc.nextLine();
-//
-//// Создание и инициализация обьектов
-//        Title titleOfBook = new Title();
-//        titleOfBook.setTitle(title);
-//
-//        Author authorOfBook = new Author();
-//        authorOfBook.setAuthor(author);
-//
-//        Content contentOfBook = new Content();
-//        contentOfBook.setContent(content);
-//
-//// Вывод значений полей обьектов
-//        titleOfBook.show();
-//        authorOfBook.show();
-//        contentOfBook.show();
-//
+    computers();
 
     }
-
 }
